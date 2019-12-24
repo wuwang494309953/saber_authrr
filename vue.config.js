44806d452c75b06a7f62f235068f8ae791ba30a1
Saber <494309953@qq.com>
2019-12-24 17:41:44 +0800

    docker改造

--- vue.config.js
+++ vue.config.js
@@ -2,20 +2,12 @@
 module.exports = {
   devServer: {
     proxy: {
-      '/v1/saberAuth': {
+      '/v1': {
         target: 'http://localhost:8080', //对应自己的接口
         changeOrigin: true,
         ws: true,
         pathRewrite: {
           '^/v1': '/v1'
-        }
-      },
-      '/v1/gateway': {
-        target: 'http://localhost:8080', //对应自己的接口
-        changeOrigin: true,
-        ws: true,
-        pathRewrite: {
-          '^/v1/gateway': ''
         }
       }
     }
